00f2e65e357a65cabd16ce6bf27169456a71638d
Константин <147883038+Kostyanych2018@users.noreply.github.com>
2024-04-05 05:19:21 +0300

    Update Main.java

--- 1.3.DataStructures/Task 2/src/com/examples/Main.java
+++ 1.3.DataStructures/Task 2/src/com/examples/Main.java
@@ -5,8 +5,8 @@ import java.util.logging.Logger;
 class Main {
     private static final Logger l = Logger.getLogger(Main.class.getName());
 
-    public static void main(String[] args) {
-        Queue<Integer> q = new Queue<>();
+    public static void main() {
+        Queue<Integer> q = new Queue<Integer>();
 
         q.enqueue(1);
         q.enqueue(2);
